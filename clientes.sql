316e0c71acf19d6603c3c345761d48349b99231f
LeandraFernanda <106930172+LeandraFernanda@users.noreply.github.com>
2024-12-18 13:15:22 -0300

    Add files via upload

--- clientes.sql
+++ clientes.sql
@@ -1,100 +1,135 @@
-create  table clientes (
-id_clientes int primary key,
-nome_clientes varchar (80) not null,
-sobrenome_clientes varchar(80) not null
-)
+select * from Clientes
+select * from Produto
+select * from pedido
+select * from Categoria
 
-create table Produto (
-id_produto int primary key,
-nome_produto varchar(80)not null,
-descricao text null,
-preco numeric check (preco > 0) not null,
-qtd_estoque smallint default 0
-)
-create table pedido(
-id_pedido serial primary key,
-fk_clientes int not null references clientes(id_clientes),
-fk_produto int not null,
-qtd_estoque smallint not null,
-foreign key (fk_produto) references produto(id_produto)
-)
-insert into clientes (id_clientes, nome_clientes, sobrenome_clientes)
-values 
- (4, 'Fernanda','Friedrich'),
- (5, 'Arthur','Ferreira'),
- (6, 'Arthur','Ferreira'),
- (7, 'Arthur','Ferreira'),
- (8, 'Daniel','Ferreira'),
- (9, 'Daniel', 'Ferreira'),
- (10, 'Daniela', 'Bial')
+select nome_clientes, qtd from Clientes, pedido
+where Clientes.id_Clientes = pedido.fk_Clientes 
 
-select * from clientes
+--exerc.1
+select nome_clientes,qtd from Clientes, pedido
+where (Clientes.id_clientes) = (pedido.qtd)
 
-insert into Produto(id_produto, nome_produto, descricao, preco, qtd_estoque)
+--exerc.2
+select fk_clientes,id_pedido  from Clientes, qtd
+where (Clientes.nome_produto) = (pedido.qtd)
+
+
+
+
+
+
+-- inner join
+-- junta id_pedido, id_produto, nome_produto e qtd
+select id_pedido, id_produto, nome_produto, qtd
+from pedido
+join Produto
+on Produto.id_produto = Produto.id_produto
+
+
+
+
+create table Categoria(
+id_categoria int primary key,
+nome_categoria varchar(80)
+);
+
+alter table Produto add id_categoria int references Categoria(id_categoria)
+
+insert into Categoria (id_categoria,nome_categoria)
 values
-(4, 'Sorvete','Sorvete de copinho',1.50,10),
-(5, 'Sorvete', 'Sorvete de 1litro', 10.00,20),
-(6, 'Refrigerante','Refrigerante de 600ml',4.00,20),
-(7, 'Refrigerante', 'Refrigerante de 1litro',7.00,20),
-(8, 'Refrigerante', 'Refrigerante de 2litros',9.50,20),
-(9, 'Refrigerante', 'refrigerante de 3litros',11.50,20),
-(10, 'Àgua', 'Àgua em garrafa de plástico de 500ml',2.50, 50)
+(1,'Brinquedo'),
+(2,'Bebida')
 
-select * from produto
+update Produto set id_categoria = 1 
+where id_produto = 1
 
-insert into pedido(fk_clientes,fk_produto,qtd)
-values (1,2,2),
-
-(4,4,1),
-(5,5,1),
-(6,6,1),
-(7,7,1),
-(8,8,1),
-(9,9,1),
-(10,10,1)
+select nome_produto, nome_categoria 
+from Produto full outer join Categoria 
+on produto.id_categoria,Categoria.id_categoria --Não deu certo, deu tipo inteiro, tem que ser boolean
 
 
- select * from pedido
-select * from clientes
+--exercicio 1 
+/*SELECT Nome_Produto, Nome_Categoria
+FROM Produto
+[LEFT] JOIN Categoria
+ON Produto.ID_Categoria = Categoria.ID_Categoria */
 
-select distinct nome_clientes from clientes
-select distinct sobrenome_clientes from clientes
+select nome_produto,nome_categoria from Categoria left join Produto 
+on Produto.nome_produto = nome_categoria
 
-alter table clientes add genero_clientes varchar(20),
-alter table clientes add salario_clientes money 
-/* Manipulação de registro na tabela/relação clientes:*/
-alter table clientes alter column cpf_clientes type varchar(11)
+--exercicio2
+/*SELECT Nome_Cliente, ID_Pedido
+FROM Cliente
+[RIGHT] JOIN Pedido
+ON Cliente.ID_Cliente = Pedido.ID_Cliente */
 
-update clientes  SET cpf_clientes = null
+  select nome_clientes, id_pedido
+  from pedido
+right join clientes
+on Clientes.id_clientes = pedido.fk_clientes 
 
-update clientes set genero_clientes = 'masculino'
+--exercicio3
+/*Exemplo: SELECT nome_colunas
+FROM nome_tabela1
+[CROSS] JOIN nome_tabela2
+[WHERE condições]
 
-update clientes set genero_clientes = 'feminino' where id_clientes = 3 
-update clientes set genero_clientes = 'feminino' where id_clientes = 4
-update clientes set genero_clientes = 'feminino' where id_clientes = 10
+Selecione todos os produtos e todos os usuários.
 
-update clientes set salario_clientes = 'R$ 1.412,00'
-update clientes set salario_clientes = 'R$ 1.500,00' where id_clientes = 3
-update clientes set salario_clientes = 'R$ 1.500,00' where id_clientes = 4
-update clientes set salario_clientes = 'R$ 1.500,00' where id_clientes = 10
-
-update clientes set cpf_clientes = '00000000000' where id_clientes = 1
-update clientes set cpf_clientes = '11111111111' where id_clientes = 2
-update clientes set cpf_clientes = '22222222222' where id_clientes = 3
-update clientes set cpf_clientes = '33333333333' where id_clientes = 4
-update clientes set cpf_clientes = '44444444444' where id_clientes = 5
-update clientes set cpf_clientes = '55555555555' where id_clientes = 6
-update clientes set cpf_clientes = '66666666666' where id_clientes = 7
-update clientes set cpf_clientes = '77777777777' where id_clientes = 8
-update clientes set cpf_clientes = '88888888888' where id_clientes = 9
-update clientes set cpf_clientes = '99999999999' where id_clientes = 10
-/* coloca em ordem alfabética*/
-SELECT * FROM clientes ORDER BY nome_clientes ASC
-
-/* o ultimo vira o primeiro e vai decrescendo*/
-SELECT * FROM clientes ORDER BY nome_clientes DESC
-/* seleciona o nome e o salario mostrando quem ganha menos de 3.000*/
-select nome_clientes,salario_clientes from clientes where genero_clientes = 'masculino' and salario_clientes <'3.000'
+SELECT Nome_Produto, Nome_Cliente
+FROM Produto
+[CROSS] JOIN Cliente */
+select nome_produto,id_categoria from Produto
+cross join Categoria 
+where Produto.id_produto =   'Brinquedo'
 
 
+
+
+
+
+
+--16/12/2024
+--View
+
+create or replace view produtos_comprados as 
+select PE.id_pedido as pedido, nome_clientes as Clientes, nome_produto as Produto, PE.qtd as quantidade,
+PE.qtd*PR.preco as fatura
+from pedido PE
+join Produto PR
+on PE.fk_produto = PR.id_produto
+join Clientes CL
+on PE.fk_clientes = CL.id_clientes
+
+select * from produtos_comprados
+select * from Produto
+select * from Categoria
+select * from pedido
+
+update produto set id_categoria = 2 where id_produto = 2
+update Produto set id_categoria = 3 where id_produto = 4
+update Produto set id_categoria = 3 where id_produto = 5
+update Produto set id_categoria = 2 where id_produto = 6
+update Produto set id_categoria = 2 where id_Produto = 7
+update Produto set id_categoria = 2 where id_produto = 8
+
+
+insert into categoria (id_categoria,nome_categoria)
+values(3,'Sorvete')
+
+--exercicio1
+create or replace view produtos_categorias as 
+select PR.id_produto as Produto, CT.nome_categoria as Categoria,  PE.qtd as quantidade,
+PE.qtd * PR.preco as fatura
+from Produto PR
+on PR.id_categoria = CT.id_categoria
+
+
+
+--Indice
+create index on Clientes (id_clientes, cpf_clientes)
+create index on Produto using hash(id_produto)
+create index on pedido using hash (id_pedido)
+create index on Clientes using hash (id_clientes)
 
